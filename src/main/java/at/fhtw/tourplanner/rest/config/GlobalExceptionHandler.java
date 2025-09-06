31b3634fbfe1097053260a279f9047bfdb454a2f
Vanessa <vanessakausl@gmail.com>
2025-09-07 01:15:51 +0200

    add global exception handling

---
+++ src/main/java/at/fhtw/tourplanner/rest/config/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package at.fhtw.tourplanner.rest.config;
+
+import at.fhtw.tourplanner.core.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(EntityNotFoundException ex) {
+        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityCreateException.class)
+    public ResponseEntity<Object> handleCreateException(EntityNotFoundException ex) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityUpdateException.class)
+    public ResponseEntity<Object> handleUpdateException(EntityNotFoundException ex) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityDeleteException.class)
+    public ResponseEntity<Object> handleDeleteException(EntityNotFoundException ex) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidEntityException.class)
+    public ResponseEntity<Object> handleInvalidException(EntityNotFoundException ex) {
+        return buildResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(ServiceRequestException.class)
+    public ResponseEntity<Object> handleServiceRequestException(EntityNotFoundException ex) {
+        return buildResponse(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage());
+    }
+
+    private ResponseEntity<Object> buildResponse(HttpStatus status, String message) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", message);
+
+        return new ResponseEntity<>(body, status);
+    }
+}
